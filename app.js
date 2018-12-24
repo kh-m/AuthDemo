11b1fbe8ecdf6e0a5c5a9b11a33ead29b494c4bc
Khaled M <k.code@icloud.com>
2018-12-24 02:11:10 -0500

    +GET:/secret -> views/secret.ejs +added&connected Mongoose

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var express = require("express");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/auth_demo_app");
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -6,5 +9,9 @@ app.set('view engine', 'ejs');
 app.get("/", function( req, res) {
     res.render("HOME");
 })
+
+app.get("/secret", function(req, res) {
+    res.render("secret")
+});
 
 app.listen(8000);
